96799ca88bd21ebef495f3b689286f33a69329f6
“Jim” <jimkernicky@gmail.com>
2022-05-28 14:44:13 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Personal Porfolio
+
+![portfolio-thumbnail](#notes)
+
+The few files and folders used to build [my portfolio page](https://kernixwebdesign.com/resume-portfolio.html).
+
+## Notes
+
+- I have a number of lines of HTML commented out as I am unsure of the Dev icons to include in my portfolio.
+- I have a link commented out in my resume-portfolio.js file where I got tips on the accessibility for a hamburger icon and for JavaScipt keyboard events
+- I also have tried linking to Google fonts and using a link in the HTML file, so you will see additional code commented out.
+
+That is about it.
---
+++ resume-portfolio.js
@@ -0,0 +1,36 @@
+// http://www.ashleysheridan.co.uk/blog/Making+an+Accessible+Hamburger+Menu
+// https://www.section.io/engineering-education/keyboard-events-in-javascript/
+
+"use strict";
+
+// Hamburger toggle
+const navToggle = document.getElementById('nav-toggle');
+const nav = document.getElementById('nav');
+
+navToggle.addEventListener("click", function (event) {
+  let menuOpen = nav.classList.contains("nav--visible");
+  let newMenuOpenStatus = !menuOpen;
+
+  navToggle.setAttribute("aria-expanded", newMenuOpenStatus);
+  nav.classList.toggle("nav--visible");
+});
+
+// Cards toggle on click and keydown
+const flip1 = document.getElementById("card1-flip");
+const flip2 = document.getElementById("card2-flip");
+const flip3 = document.getElementById("card3-flip");
+const flip4 = document.getElementById("card4-flip");
+
+const cards = document.querySelectorAll('.card');
+const cardsId = [flip1, flip2, flip3, flip4];
+
+cards.forEach(card => card.addEventListener('click', () => card.classList.toggle('show-answer')));
+
+cardsId.forEach(function (card) {
+  card.addEventListener('keydown', function (e) {
+    if (e.code !== 'Tab') {
+      e.preventDefault();
+      card.classList.toggle('show-answer');
+    }
+  });
+});

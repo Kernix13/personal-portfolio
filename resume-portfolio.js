d6d13253dc1493b85e2e60e6826077a3ab0b445b
“Jim” <jimkernicky@gmail.com>
2022-08-09 20:51:03 -0400

    React project

--- resume-portfolio.js
+++ resume-portfolio.js
@@ -4,8 +4,8 @@
 "use strict";
 
 // Hamburger toggle
-const navToggle = document.getElementById('nav-toggle');
-const nav = document.getElementById('nav');
+const navToggle = document.getElementById("nav-toggle");
+const nav = document.getElementById("nav");
 
 navToggle.addEventListener("click", function (event) {
   let menuOpen = nav.classList.contains("nav--visible");
@@ -19,18 +19,19 @@ navToggle.addEventListener("click", function (event) {
 const flip1 = document.getElementById("card1-flip");
 const flip2 = document.getElementById("card2-flip");
 const flip3 = document.getElementById("card3-flip");
-const flip4 = document.getElementById("card4-flip");
+// const flip4 = document.getElementById("card4-flip");
+const flip5 = document.getElementById("card5-flip");
 
-const cards = document.querySelectorAll('.card');
-const cardsId = [flip1, flip2, flip3, flip4];
+const cards = document.querySelectorAll(".card");
+const cardsId = [flip1, flip2, flip3, flip5];
 
-cards.forEach(card => card.addEventListener('click', () => card.classList.toggle('show-answer')));
+cards.forEach(card => card.addEventListener("click", () => card.classList.toggle("show-answer")));
 
 cardsId.forEach(function (card) {
-  card.addEventListener('keydown', function (e) {
-    if (e.code !== 'Tab') {
+  card.addEventListener("keydown", function (e) {
+    if (e.code !== "Tab") {
       e.preventDefault();
-      card.classList.toggle('show-answer');
+      card.classList.toggle("show-answer");
     }
   });
 });

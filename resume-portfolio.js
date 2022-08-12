a0066f17855484e997645506383e562866531c8c
“Jim” <jimkernicky@gmail.com>
2022-08-12 12:15:00 -0400

    Details page and CSS/JS changes

--- resume-portfolio.js
+++ resume-portfolio.js
@@ -1,7 +1,4 @@
 // http://www.ashleysheridan.co.uk/blog/Making+an+Accessible+Hamburger+Menu
-// https://www.section.io/engineering-education/keyboard-events-in-javascript/
-
-"use strict";
 
 // Hamburger toggle
 const navToggle = document.getElementById("nav-toggle");
@@ -13,25 +10,4 @@ navToggle.addEventListener("click", function (event) {
 
   navToggle.setAttribute("aria-expanded", newMenuOpenStatus);
   nav.classList.toggle("nav--visible");
-});
-
-// Cards toggle on click and keydown
-const flip1 = document.getElementById("card1-flip");
-const flip2 = document.getElementById("card2-flip");
-const flip3 = document.getElementById("card3-flip");
-// const flip4 = document.getElementById("card4-flip");
-const flip5 = document.getElementById("card5-flip");
-
-const cards = document.querySelectorAll(".card");
-const cardsId = [flip1, flip2, flip3, flip5];
-
-cards.forEach(card => card.addEventListener("click", () => card.classList.toggle("show-answer")));
-
-cardsId.forEach(function (card) {
-  card.addEventListener("keydown", function (e) {
-    if (e.code !== "Tab") {
-      e.preventDefault();
-      card.classList.toggle("show-answer");
-    }
-  });
 });
